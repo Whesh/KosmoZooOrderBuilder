c1f353177f48b81231e3a11ff8d8e65d7957a60f
Whesh <mr220985edv@whesh>
2017-09-29 16:02:41 +0300

    Create model for Demetra company price.

---
+++ src/main/java/com/whesh/xlsorderbuilder/model/PriceDemetra.java
@@ -0,0 +1,18 @@
+package com.whesh.xlsorderbuilder.model;
+
+public class PriceDemetra extends AbstractPrice{
+    @Override
+    public void setSellerName() {
+        sellerName = "Общество с ограниченной ответственностью «Компания ДЕМЕТРА»";
+    }
+
+    @Override
+    public void setProductIdCellNumber() {
+        productIdCellNumber = 3;
+    }
+
+    @Override
+    public void setProductCollsCellNumber() {
+        productCollsCellNumber = 12;
+    }
+}
